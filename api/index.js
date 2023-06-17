4376f2532895f86cdf5d38b9da28290e0841b306
wiewior04 <66681126+wiewior04@users.noreply.github.com>
2023-06-17 18:10:40 +0200

    Added bcryptjs for password encryption

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
 const app = express();
+const bcrypt = require('bcryptjs');
+
+const salt = 'sagsdaogawj253154';
 
 app.use(cors());
 app.use(express.json());
@@ -11,8 +14,15 @@ mongoose.connect('mongodb+srv://blog:V8zS3dy5NndSXPKm@cluster0.qufmn6d.mongodb.n
 
 app.post('/register', async (req,res) => {
     const {username,password} = req.body;
-    const userDoc = await User.create({username,password});
-    res.json(userDoc);
+    try {
+        const userDoc = await User.create({
+            username,
+            password:bcrypt.hashSync(password,salt),
+        });
+        res.json(userDoc);
+    }  catch(e) {
+        res.status(400).json(e);
+    }
 });
 
 app.listen(4000)
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -4,12 +4,17 @@ export default function RegisterPage() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     async function register(ev){
-        ev.preventDefault();
-        fetch('http://localhost:4000/register', {
+    ev.preventDefault();
+    const response = await fetch('http://localhost:4000/register', {
             method: 'POST',
             body: JSON.stringify({username,password}),
             headers: {'Content-Type':'application/json'}
         });
+    if (response.status === 200) {
+        alert('Registration successful');
+    } else {
+        alert('Registration failed');
+    }
     }
     return (
         <form className="register" onSubmit={register}>
