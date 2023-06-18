6111603f478103db0932e964d42a732a5ad50e94
wiewior04 <66681126+wiewior04@users.noreply.github.com>
2023-06-18 22:41:00 +0200

    Added logout functionality

--- api/index.js
+++ api/index.js
@@ -5,12 +5,14 @@ const User = require('./models/User');
 const app = express();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const salt = bcrypt.genSaltSync(10);
 const secret = '3qi3 jt02   j 5q43qq34g';
 
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect('mongodb+srv://blog:V8zS3dy5NndSXPKm@cluster0.qufmn6d.mongodb.net/?retryWrites=true&w=majority');
 
@@ -40,6 +42,18 @@ app.post('/login', async (req,res) => {
     } else {
         res.status(400).json('wrong credentials');
     }
+});
+
+app.get('/profile', (req, res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err,info) => {
+        if (err) throw err;
+        res.json(info);
+    })    
+});
+
+app.post('/logout', (req,res) => {
+    res.cookie('token', '').json('ok');
 });
 
 app.listen(4000)
