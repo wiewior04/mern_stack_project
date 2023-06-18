0d833ab06b156cfdd3067b67d69bcf1b92351eb8
wiewior04 <66681126+wiewior04@users.noreply.github.com>
2023-06-18 19:07:03 +0200

    finish login logic

--- api/index.js
+++ api/index.js
@@ -4,10 +4,12 @@ const mongoose = require('mongoose');
 const User = require('./models/User');
 const app = express();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const salt = bcrypt.genSaltSync(10);
+const secret = '3qi3 jt02   j 5q43qq34g';
 
-app.use(cors());
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://blog:V8zS3dy5NndSXPKm@cluster0.qufmn6d.mongodb.net/?retryWrites=true&w=majority');
@@ -22,6 +24,21 @@ app.post('/register', async (req,res) => {
         res.json(userDoc);
     }  catch(e) {
         res.status(400).json(e);
+    }
+});
+
+app.post('/login', async (req,res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+        // logged in
+        jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+            if (err) throw err;
+            res.cookie('token', token).json('ok');
+        });
+    } else {
+        res.status(400).json('wrong credentials');
     }
 });
 
