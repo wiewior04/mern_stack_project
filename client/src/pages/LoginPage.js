79a8837bb35b6ab607ed3d35e97757bc01566ce7
wiewior04 <66681126+wiewior04@users.noreply.github.com>
2023-06-18 18:39:19 +0200

    working with login

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,9 +1,12 @@
 export default function LoginPage(){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     return(
         <form className="login">
             <h1>Login</h1>
-            <input type="text" placeholder="username"/>
-            <input type="password" placeholder="password"/>
+            <input type="text" placeholder="username" value={username} onChange={ev => setUsername(ev.target.value)}/>
+            <input type="password" placeholder="password" value={password} onChange={ev => setPassword(ev.target.value)}/>
             <button>Login</button>
         </form>
     );
