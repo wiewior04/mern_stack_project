d6cfd51a94846fa77a29ef2d63341637a129d450
wiewior04 <66681126+wiewior04@users.noreply.github.com>
2023-06-19 21:58:53 +0200

    working with create post functionality

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,17 @@ import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage.js';
 import {UserContextProvider, UserContext} from './UserContext';
+import CreatePost from './pages/CreatePost';
 
 function App() {
   return (
     <UserContextProvider>
       <Routes>
         <Route path="/" element={<Layout />}>
-         <Route index element={<IndexPage />} />
-         <Route path={'/login'} element={<LoginPage />} />
-          <Route path={'/register'} element={<RegisterPage />} />
+        <Route index element={<IndexPage />} />
+        <Route path={'/login'} element={<LoginPage />} />
+        <Route path={'/register'} element={<RegisterPage />} />
+        <Route path={'/create'} element={<CreatePost />} />
         </Route>
       </Routes>
     </UserContextProvider>
