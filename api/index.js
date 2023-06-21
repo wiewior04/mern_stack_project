d59811d93230c0036470da6032203a9f14a34ae6
wiewior04 <66681126+wiewior04@users.noreply.github.com>
2023-06-21 14:12:55 +0200

    added multer

--- api/index.js
+++ api/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
+const Post = require('./models/Post');
 const app = express();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/' });
+const fs = require('fs');
 
 const salt = bcrypt.genSaltSync(10);
 const secret = '3qi3 jt02   j 5q43qq34g';
@@ -58,6 +62,24 @@ app.get('/profile', (req, res) => {
 app.post('/logout', (req,res) => {
     res.cookie('token', '').json('ok');
 });
+
+app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
+    const {originalname,path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path+'.'+ext;
+    fs.renameSync(path, newPath);
+
+    const {title,summary,content} = req.body;
+    const postDoc = await Post.create({
+        title,
+        summary,
+        content,
+        cover:newPath,
+    })
+
+    res.json(postDoc);
+})
 
 app.listen(4000)
 
